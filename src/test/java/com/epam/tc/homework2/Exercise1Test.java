90e10b3783b0a34182615ddba514c717d34dee53
anna_ryabusheva <anna_ryabusheva@epam.com>
2022-02-26 17:33:01 +0300

    Exercise 1 and Exercise 2

---
+++ src/test/java/com/epam/tc/homework2/Exercise1Test.java
@@ -0,0 +1,188 @@
+package com.epam.tc.homework2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.DURATION;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Exercise1Test {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    // 12.Close Browser
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testOpenPage() {
+        SoftAssertions softly = new SoftAssertions();
+
+        //
+        driver.manage().window().maximize();
+        // 1.  Open test site by URL
+        driver.get("https://jdi-testing.github.io/jdi-light/index.html ");
+        // 2. Assert Browser title "Home Page"
+        softly.assertThat(driver.getTitle()).isEqualTo("Home Page");
+        //   Find login's form and click
+        driver.findElements(By.className("navbar-right")).get(0).click();
+        // 3. Perform login
+        driver.findElement(By.id("name")).sendKeys("Roman");
+        driver.findElement(By.id("password")).sendKeys("Jdi1234");
+
+        // Click to bottom Enter
+        driver.findElement(By.id("login-button")).click();
+        // 4.Assert Username is logging
+        String txtUserName = driver.findElement(By.id("user-name")).getText();
+        softly.assertThat(txtUserName).isEqualTo("ROMAN IOVLEV");
+        // 5.Assert that there are 4 items on the header section are displayed,
+        // and they have proper texts( element= Home)
+
+        String fieldHome = driver.findElement(
+            By.xpath("//ul[@class=\"uui-navigation nav navbar-nav m-l8\"]//*[contains(text(),'Home')]")).getText();
+        softly.assertThat(fieldHome).isEqualTo("HOME");
+
+        // 5. Assert that there are 4 items on the header section are displayed,
+        // and they have proper texts ( element = CONTACT FORM )
+
+        String fieldContactForm = driver.findElement(
+                                            By.xpath("//ul[@class=\"uui-navigation nav navbar-nav m-l8\"]"
+                                                + "//*[contains(text(),'Contact form')]"))
+                                        .getText();
+        softly.assertThat(fieldContactForm).isEqualTo("CONTACT FORM");
+
+        // 5. Assert that there are 4 items on the header section are displayed,
+        // and they have proper texts ( element = SERVICE )
+
+        String fieldService = driver.findElement(
+            By.xpath("//ul[@class=\"uui-navigation nav navbar-nav m-l8\"]//*[contains(text(),'Service')]")).getText();
+        softly.assertThat(fieldService).isEqualTo("SERVICE");
+        // 5.Assert that there are 4 items on the header section are displayed,
+        // and they have proper texts ( element = METALS & COLORS )
+        // "METALS & COLORS");
+        String fieldMetalColor = driver.findElement(
+                                           By.xpath("//ul[@class=\"uui-navigation nav navbar-nav m-l8\"]"
+                                               + "//*[contains(text(),'Metals & Colors')]"))
+                                       .getText();
+        softly.assertThat(fieldMetalColor).isEqualTo("METALS & COLORS");
+
+        // 6.Assert that there are 4 images on the Index Page, and they are displayed
+
+        boolean image = driver.findElement(By.className("icon-practise")).isDisplayed();
+        softly.assertThat(image).isTrue();
+        boolean imageCustom = driver.findElement(By.className("icon-custom")).isDisplayed();
+        softly.assertThat(imageCustom).isTrue();
+        boolean imageBenefit = driver.findElement(By.className("icon-multi")).isDisplayed();
+        softly.assertThat(imageBenefit).isTrue();
+        boolean imageTxt = driver.findElement(By.className("icon-base")).isDisplayed();
+        softly.assertThat(imageTxt).isTrue();
+        // 7.Assert that there are 4 texts on the Index Page under icons,
+        // and they have proper text,4 texts below of each image
+        // Texts are displayed and equal to expected
+        List<WebElement> itemsTR =
+            driver.findElements(By.xpath("//div[@class=\"row clerafix benefits\"]/div[@class=\"col-sm-3\"]"));
+        int itemTR1 = itemsTR.size();
+        System.out.println(itemTR1);
+        List<WebElement> lst =
+            driver.findElements(By.xpath("//div[@class=\"row clerafix benefits\"]/div[@class=\"col-sm-3\"]"));
+        List<String> strings = new ArrayList<>();
+        for (WebElement e : lst) {
+            strings.add(e.getText());
+        }
+        List<String> list = new ArrayList<>();
+        list.add("To include good practices\nand ideas from successful\nEPAM project");
+        list.add("To be flexible and\ncustomizable");
+        list.add("To be multiplatform");
+        list.add("Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
+        boolean b = list.containsAll(strings);
+        softly.assertThat(b).isTrue();
+        // 8.Assert that there is the iframe with “Frame Button” exist
+        driver.switchTo().frame(driver.findElement(By.id("frame")));
+        // 9.Switch to the iframe and check that there is “Frame Button” in the iframe
+        boolean buttonIsExist = driver.findElement(By.xpath("//*[@id=\"frame-button\"]")).isDisplayed();
+        softly.assertThat(buttonIsExist).isTrue();
+        // 10.Switch to original window back
+        driver.switchTo().defaultContent();
+
+        // 11.Assert that there are 5 items in the Left Section are displayed, and they have proper text
+        boolean leftHome = driver.findElement(By.xpath(
+                                     "//div[@name=\"navigation-sidebar\"]"
+                                         + "//ul[@class=\"sidebar-menu left\"]//*[contains(text(),'Home')]"))
+                                 .isDisplayed();
+        softly.assertThat(leftHome).isTrue();
+        String leftHomeContTxt = driver.findElement(By.xpath(
+                                           "//div[@name=\"navigation-sidebar\"]"
+                                               + "//ul[@class=\"sidebar-menu left\"]//*[contains(text(),'Home')]"))
+                                       .getText();
+        softly.assertThat(leftHomeContTxt).isEqualTo("Home");
+        boolean leftContactForm = driver.findElement(By.xpath(
+                                            "//div[@name=\"navigation-sidebar\"]"
+                                                + "//ul[@class=\"sidebar-menu left\"]"
+                                                + "//*[contains(text(),'Contact form')]"))
+                                        .isDisplayed();
+        softly.assertThat(leftContactForm).isTrue();
+        String leftContactFormTxt = driver.findElement(By.xpath(
+                                              "//div[@name=\"navigation-sidebar\"]"
+                                                  + "//ul[@class=\"sidebar-menu left\"]"
+                                                  + "//*[contains(text(),'Contact form')]"))
+                                          .getText();
+        softly.assertThat(leftContactFormTxt).isEqualTo("Contact form");
+        boolean leftService = driver.findElement(By.xpath(
+                                        "//div[@name=\"navigation-sidebar\"]"
+                                            + "//ul[@class=\"sidebar-menu left\"]//*[contains(text(),'Service')]"))
+                                    .isDisplayed();
+        softly.assertThat(leftService).isTrue();
+        String leftServiceConTxt = driver.findElement(By.xpath(
+                                             "//div[@name=\"navigation-sidebar\"]"
+                                                 + "//ul[@class=\"sidebar-menu left\"]"
+                                                 + "//*[contains(text(),'Service')]"))
+                                         .getText();
+        softly.assertThat(leftServiceConTxt).isEqualTo("Service");
+        boolean leftMetalColor = driver.findElement(By.xpath(
+                                           "//div[@name=\"navigation-sidebar\"]"
+                                               + "//ul[@class=\"sidebar-menu left\"]"
+                                               + "//*[contains(text(),'Metals & Colors')]"))
+                                       .isDisplayed();
+        softly.assertThat(leftMetalColor).isTrue();
+        String leftMetalColConTxt = driver.findElement(By.xpath(
+                                              "//div[@name=\"navigation-sidebar\"]"
+                                                  + "//ul[@class=\"sidebar-menu left\"]"
+                                                  + "//*[contains(text(),'Metals & Colors')]"))
+                                          .getText();
+        softly.assertThat(leftMetalColConTxt).isEqualTo("Metals & Colors");
+        boolean leftElementsPacks = driver.findElement(By.xpath(
+                                              "//div[@name=\"navigation-sidebar\"]"
+                                                  + "//ul[@class=\"sidebar-menu left\"]"
+                                                  + "//*[contains(text(),'Elements packs')]"))
+                                          .isDisplayed();
+        softly.assertThat(leftElementsPacks).isTrue();
+        String leftElementsPConTxt = driver.findElement(By.xpath(
+                                               "//div[@name=\"navigation-sidebar\"]"
+                                                   + "//ul[@class=\"sidebar-menu left\"]"
+                                                   + "//*[contains(text(),'Elements packs')]"))
+                                           .getText();
+        softly.assertThat(leftElementsPConTxt).isEqualTo("Elements packs");
+    }
+}
+
+
---
+++ src/test/java/com/epam/tc/homework2/Exercise2Test.java
@@ -0,0 +1,148 @@
+package com.epam.tc.homework2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Exercise2Test {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    // 12.Close Browser
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testOpenPage() {
+        SoftAssertions softly = new SoftAssertions();
+        driver.manage().window().maximize();
+        //1. Open test site by URL
+        driver.get("https://jdi-testing.github.io/jdi-light/index.html ");
+        //2. Assert Browser title "Home Page"
+        softly.assertThat(driver.getTitle()).isEqualTo("Home Page");
+        // Find login's form and click
+        driver.findElements(By.className("navbar-right")).get(0).click();
+        // 3.Perform login
+        driver.findElement(By.id("name")).sendKeys("Roman");
+        driver.findElement(By.id("password")).sendKeys("Jdi1234");
+
+        // Click to bottom Enter
+        driver.findElement(By.id("login-button")).click();
+        // 4. Assert Username is logging
+        String txtUserName = driver.findElement(By.id("user-name")).getText();
+        softly.assertThat(txtUserName).isEqualTo("ROMAN IOVLEV");
+
+        // 5.Open through the header menu Service -> Different Elements Page
+        WebElement proba = driver.findElement(By.xpath("//div[@class=\"uui-header dark-gray\"]"
+            + "//a[@data-toggle=\"dropdown\"]"));
+        proba.click();
+
+        WebElement dropdown = driver.findElement(By.xpath("//ul[@class=\"dropdown-menu\"]/li[8]"));
+        dropdown.click();
+
+        String differentPageOpen = driver.findElement(By.xpath("//div[@class=\"support-title\"]")).getText();
+        softly.assertThat(differentPageOpen).isEqualTo("Support");
+
+        // 6.Select checkboxes Water
+        driver.findElement(By.xpath("//label[@class=\"label-checkbox\"][1]/input")).click();
+        String verifyWater =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyWater).contains("Water: condition changed to true");
+        // 6.Select checkboxes Wind
+        driver.findElement(By.xpath("//label[@class=\"label-checkbox\"][3]/input")).click();
+        String verifyWind =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyWind).contains("Wind: condition changed to true");
+
+        // 7.Select radio Selen Element is checked
+        driver.findElement(By.xpath("//label[@class=\"label-radio\"][4]/input")).click();
+        String verifySelen =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifySelen).contains("metal: value changed to Selen");
+
+        // 8.Select in dropdown Yellow Element is selected
+
+        WebElement selectElem = driver.findElement(By.xpath("//div[@class=\"colors\"]"
+            + "//select[@class=\"uui-form-element\"]"));
+        Select selectColor = new Select(selectElem);
+        selectColor.selectByVisibleText("Yellow");
+        String verifyColor =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyColor).contains("Colors: value changed to Yellow");
+        // 9.Assert that
+        // for each checkbox there is an individual log row and value is corresponded to the status of checkbox
+        // for radio button there is a log row and value is corresponded to the status of radio button
+        // for dropdown there is a log row and value is corresponded to the selected value.
+        driver.findElement(By.xpath("//label[@class=\"label-checkbox\"][2]/input")).click();
+        String verifyEarth =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyEarth).contains("Earth: condition changed to true");
+        driver.findElement(By.xpath("//label[@class=\"label-checkbox\"][4]/input")).click();
+        String verifyFire =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyFire).contains("Fire: condition changed to true");
+        driver.findElement(By.xpath("//label[@class=\"label-radio\"][1]/input")).click();
+        String verifyGold =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyGold).contains("metal: value changed to Gold");
+        driver.findElement(By.xpath("//label[@class=\"label-radio\"][2]/input")).click();
+        String verifySilver =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifySilver).contains("metal: value changed to Silver");
+        driver.findElement(By.xpath("//label[@class=\"label-radio\"][3]/input")).click();
+        String verifyBronze =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyBronze).contains("metal: value changed to Bronze");
+        WebElement selectColorR = driver.findElement(By.xpath("/html/body/div/div[2]/main/div[2]/div/div[4]/select"));
+        Select selectColorRed = new Select(selectColorR);
+        selectColorRed.selectByVisibleText("Red");
+        String verifyColorRed =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyColorRed).contains("Colors: value changed to Red");
+        WebElement selectColorG = driver.findElement(By.xpath("/html/body/div/div[2]/main/div[2]/div/div[4]/select"));
+        Select selectColorGreen = new Select(selectColorG);
+        selectColorGreen.selectByVisibleText("Green");
+        String verifyColorGreen =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyColorGreen).contains("Colors: value changed to Green");
+        WebElement selectColorB = driver.findElement(By.xpath("/html/body/div/div[2]/main/div[2]/div/div[4]/select"));
+        Select selectColorBlue = new Select(selectColorB);
+        selectColorBlue.selectByVisibleText("Blue");
+        String verifyColorBlue =
+            driver.findElement(By.xpath("//div[@name=\"log-sidebar\"]//ul[@class=\"panel-body-list logs\"]/li[1]"))
+                  .getText();
+        softly.assertThat(verifyColorBlue).contains("Colors: value changed to Blue");
+    }
+}
+
+
